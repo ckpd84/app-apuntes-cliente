080bea7cbcd0864e95763130251f4193c80d99ab
ckpd84 <ckpd84@gmail.com>
2021-04-21 14:39:14 +0200

    maquetando...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-
 function App() {
-  return (
-   <h1>Apuntes</h1>
-  );
+	return (
+		<div class='h-screen grid grid-rows-6 grid-flow-col'>
+			<div class='w-16 row-span-6 bg-purple-200'>aside</div>
+			<div class='row-span-1  bg-pink-200'>nav</div>
+			<div class='row-span-5   bg-blue-200'>main</div>
+		</div>
+	);
 }
 
 export default App;
