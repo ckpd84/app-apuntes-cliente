9bc618aee240ed516e0468adf747df046ea452c5
ckpd84 <ckpd84@gmail.com>
2021-04-22 16:05:37 +0200

    edito componente fecha

--- src/components/layout/aside/Fecha.js
+++ src/components/layout/aside/Fecha.js
@@ -1,13 +1,46 @@
 import React from "react";
 
 const Fecha = () => {
-	const diaActual = new Date();
-	console.log(diaActual);
+	const fechaActual = new Date();
+	const mesActual = fechaActual.getMonth();
+	const dia = fechaActual.getDate();
+	const anho = fechaActual.getFullYear();
+	let mesNombre = "";
+	let diaDeLaSemana = "";
+	const meses = [
+		"Enero",
+		"Febrero",
+		"Marzo",
+		"Abril",
+		"Mayo",
+		"Junio",
+		"Julio",
+		"Agosto",
+		"Septiembre",
+		"Octubre",
+		"Noviembre",
+		"Diciembre",
+	];
+	const dias = [
+		"Domingo",
+		"Lunes",
+		"Martes",
+		"Miércoles",
+		"Jueves",
+		"Viernes",
+		"Sábado",
+	];
+
+	meses.map((mes, i) => {
+		if (mesActual === i) mesNombre = mes;
+	});
+	dias.map((dia, i) => {
+		if (fechaActual.getDay() === i) diaDeLaSemana = dia;
+	});
+
 	return (
-		<div>
-			{diaActual.getDate()}
-			{diaActual.getMonth()}
-			{diaActual.getFullYear()}
+		<div className='mb-4'>
+			<p className='font-medium'>{`${diaDeLaSemana}, ${dia} de ${mesNombre} de ${anho}`}</p>
 		</div>
 	);
 };
